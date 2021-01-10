88f07d754970f64c842198a9e3ced80e9267147c
samuelceron <samceronm@gmail.com>
2021-01-09 19:07:38 -0600

    UFO-level-1 complete table

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,22 @@
+// from data.js
+var tableData = data;
+
+// Set the table body
+var tbody = d3.select("tbody");
+
+// Set UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row `tr` for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+
