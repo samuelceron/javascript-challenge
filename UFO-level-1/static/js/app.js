763d63b9c4881ce4f3b91d8b67066f68e486d7ad
samuelceron <samceronm@gmail.com>
2021-01-09 19:48:43 -0600

    Building handler function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,3 +20,35 @@ tableData.forEach(function(ufoSighting) {
 });
 
 
+// Set the button
+var button = d3.select("#filter-btn");
+
+//Build function on click
+button.on("click", function() {
+    tbody.html("");
+
+    // Set input data
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    // console.log(inputValue);
+    
+    // Filter data 
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    //console.log(filteredData);
+
+
+    filteredData.forEach(function(selections) {
+
+        console.log(selections);
+        // Append one table row `tr` for each UFO Sighting object
+        var row = tbody.append("tr");
+        // Use `Object.entries` to console.log each UFO Sighting value
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+            // Append a cell to the row for each value
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
+
